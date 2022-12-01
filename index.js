86af98691b83eb1f2c3b5d7aef160b366695a229
Hdesai97 <hdesai97@gmail.com>
2022-12-01 14:34:23 -0500

    initial

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require('dotenv').config()
 const express = require ('express')
 const app= express()
 
+app.use('/places', require ('./controllers/placesg'))
 
 app.get('/', (req, res) => {
-
     res.send('Hello World')
 })
 
+app.get('*', (req,res) => {
+    res.status(404).send('<h1> 404 Page </h1>')
+})
 
-app.listen(3000)
+
+app.listen(process.env.PORT)
