476f320a4e681187c1ad44cfc32b0e4fff3dd98d
Hdesai97 <hdesai97@gmail.com>
2022-12-09 12:58:00 -0500

    Adding styles to Rest-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,9 @@ function Def (html) {
     <html>
         <head>
                 <title>Title</title>
+                
+            /* https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css *\
+                <link rel="stylesheet" href="/css/style.css"/>
         </head>
         <body>
             {html.children}
